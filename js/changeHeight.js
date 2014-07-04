ef7eb5e535001a5d9e4e36df1ac6244236e4cbc6
Blaine Fricks <blaine.fricks@live.com>
2014-07-03 19:46:10 -0700

    v0.1.14

--- js/changeHeight.js
+++ js/changeHeight.js
@@ -1,13 +1,17 @@
 //hange Height based on Window Height
 function changeHeight() {
     var windowHeight = $(window).height();
+    var windowHeight50 = windowHeight * 50/100;
     var footerHeight = $("footer").height();
     $(".window-height").css({
         "height": windowHeight
     });
+    $(".window-height50").css({
+        "height": windowHeight50
+    });
     $(".page-holder").css({
       "padding-bottom" : footerHeight + 1,
-      "min-height": windowHeight * 2
+      "min-height": windowHeight
     });
 }
 window.addEventListener('resize', changeHeight);
