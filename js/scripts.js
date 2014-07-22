9e0de38f294f3562ad44012b10ec3aeaae45d41e
bfricks <bfricks@cequint.com>
2014-07-21 17:35:04 -0700

    v0.1.27

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
     $("html, body").animate({
       scrollTop:$("#scrollTo").offset().top},600)
   });
+  $("#scrollToProjects").click(function(){
+    $("html, body").animate({
+      scrollTop:$("#projects").offset().top},600)
+  });
+  $("#scrollToBlog").click(function(){
+    $("html, body").animate({
+      scrollTop:$("#blog").offset().top},600)
+  });
   // Parallax
   $(window).scroll(function() {
     var scrollAmount = $(window).scrollTop() / 5;
