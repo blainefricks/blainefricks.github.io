8defa1f916abe37a5269b7386edcfddd16b8f04c
Blaine Fricks <blaine.fricks@live.com>
2014-08-22 16:26:29 -0700

    small changes

--- js/scripts.js
+++ js/scripts.js
@@ -50,22 +50,22 @@ stickyNav();
 
 // Change Height based on Window Height
 function changeHeight() {
-    var windowHeight = $(window).height();
-    var windowHeight50 = windowHeight * 50/100;
-    var footerHeight = $("footer").height();
-    $(".window-height").css({
-        "height": windowHeight
-    });
-    $(".window-height50").css({
-        "height": windowHeight50
-    });
-    $(".page-holder").css({
-      "padding-bottom" : footerHeight + 1,
-      "min-height": windowHeight
-    });
-    $("section.blog-hero").css({
-        "min-height" : .75 * windowHeight
-    })
+  var windowHeight = $(window).height();
+  var windowHeight50 = windowHeight * 50/100;
+  var footerHeight = $("footer").height();
+  $(".window-height").css({
+      "height": windowHeight
+  });
+  $(".window-height50").css({
+      "height": windowHeight50
+  });
+  $(".page-holder").css({
+    "padding-bottom" : footerHeight + 1,
+    "min-height": windowHeight
+  });
+  $("section.blog-hero").css({
+      "min-height" : .75 * windowHeight
+  })
 }
 window.addEventListener('resize', changeHeight);
 changeHeight();
